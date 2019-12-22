227a32512c3f3400c63895c9c42357e0d8926acf
root <root@DESKTOP-9AMNB8N.localdomain>
2019-12-22 13:41:56 -0500

    add some asyn code

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,58 @@
 import './components/css/App.css';
 import Nav from './components/Navbar';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 // Main pages
 import Graph from './components/Graph';
 import Login from './components/Login';
 import About from './components/About';
 import Welcome from './components/Welcome';
+import Post from './components/Post';
+import axios from 'axios';
 
 var React = require('react');
 // var Nav = require('./components/Navbar');
 
 class App extends React.Component {
+  state = {
+    posts: [],
+    curPost: null
+  }
+
+  componentDidMount() {
+    console.log('App mounted');
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(
+      (res) => {
+        console.log(res);
+        this.setState({
+          posts: res.data.slice(0, 10)
+        })
+      });
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Component Did Updated: App');
+  }
+
+
+
   render() {
+    console.log('App rendered');
+
+    const { posts } = this.state;
+    const poList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div key={post.id}>
+            <Link to={"/" + post.id}>
+              {post.id} : {post.title}
+            </Link>
+          </div>
+        )
+      })
+    ) : (
+        <div>No Post</div>
+      );
     return (
       <BrowserRouter>
         <Nav />
@@ -20,7 +61,10 @@ class App extends React.Component {
           <Route exact path="/graph" component={Graph}></Route>
           <Route exact path="/login" component={Login}></Route>
           <Route exact path="/about" component={About}></Route>
+          <Route exact path="/:post_id" component={Post}></Route>
         </Switch>
+        <h2> Accouncements </h2>
+        {poList}
       </BrowserRouter>
     );
   }
